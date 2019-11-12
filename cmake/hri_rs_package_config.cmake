76d55fbd0475c7c9b1b5e8b1dba700f05adcc24c
Guillaume Sarthou <gsarthou@laas.fr>
2019-11-12 08:32:54 +0100

    initial commit

---
+++ cmake/hri_rs_package_config.cmake
@@ -0,0 +1,6 @@
+set(hri_rs_package_NAMESPACE           hri_rs_package)
+set(hri_rs_package_TYPESYSTEM_CPP_PATH /home/gsarthou/Robots/Pr2/Sherlock/catkin_ws/src/hri_rs_package/include/hri_rs_package/types)
+set(hri_rs_package_TYPESYSTEM_XML_PATH /home/gsarthou/Robots/Pr2/Sherlock/catkin_ws/src/hri_rs_package/descriptors/typesystem)
+set(hri_rs_package_ANNOTATOR_PATH      /home/gsarthou/Robots/Pr2/Sherlock/catkin_ws/src/hri_rs_package/descriptors/annotators)
+set(hri_rs_package_ENGINE_PATH         /home/gsarthou/Robots/Pr2/Sherlock/catkin_ws/src/hri_rs_package/descriptors/analysis_engines)
+
